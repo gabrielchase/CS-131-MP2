013c511a57db10c2d3ef0f75822b1c26d2888fc7
gabrielchase <gchase.patron@gmail.com>
2019-05-03 21:17:26 +0800

    Add comments

--- 1a.py
+++ 1a.py
@@ -8,6 +8,16 @@ Michael Pio Mayol
 CS 131 THU
 '''
 def newton_raphson(a, m, n, x=1, desired_percent_error=.01):
+    '''
+    input: integer a, integer m, integer n, 
+    optional inputs: integer x, float desired_percent_error
+
+    output: integer x
+
+    Applies the newton raphson method to get a^(m/n). Continully does this
+    until percent_error is less than the desired_percent_error
+    '''
+
     percent_error = 1
     while (percent_error >= desired_percent_error):
         xnp1 = x - (compute_fx(x, n, a, m) / compute_dfx(x, n, a, m))
@@ -16,18 +26,42 @@ def newton_raphson(a, m, n, x=1, desired_percent_error=.01):
     return x
 
 def get_percent_error(xnp1, xn):
+    '''
+    input: integer xnp1, xn
+    output: float 
+
+    Calculates the percentage error
+    '''
     return abs(((xnp1 - xn) / xnp1))
 
 def fake_exponent(base, exp):
+    '''
+    input: integer base, integer exp
+    output: integer b
+
+    Uses for loop to get the value of the exponent, base^exp
+    '''
     b = 1 
     for i in range(exp):
         b *= base 
     return b
 
 def compute_fx(x, n, a, m):
+    '''
+    input: integer x, integer n, integer a, integer m
+    output: integer
+
+    Computes f(x) = x^n - a^m
+    '''
     return fake_exponent(x, n) - fake_exponent(a, m)
 
 def compute_dfx(x, n, a, m):
+     '''
+    input: integer x, integer n, integer a, integer m
+    output: integer
+
+    Computes the derivative of f'(x) = n * (x)^(n-1)
+    '''
     return (n)*(fake_exponent(x, n-1))
 
 if __name__ == '__main__':
--- 1b.py
+++ 1b.py
@@ -10,6 +10,16 @@ CS 131 THU
 import math
 
 def newton_raphson(a=0, x=.5, desired_percent_error=.01):
+     '''
+    input: integer a
+    optional inputs: integer x, float desired_percent_error
+
+    output: integer x
+
+    Applies the newton raphson method to get arcsin(a). Continully does this
+    until percent_error is less than the desired_percent_error
+    '''
+
     percent_error = 1
     
     while (percent_error >= desired_percent_error):
@@ -20,12 +30,30 @@ def newton_raphson(a=0, x=.5, desired_percent_error=.01):
     return x
 
 def get_percent_error(xnp1, xn):
+     '''
+    input: integer xnp1, xn
+    output: float 
+
+    Calculates the percentage error
+    '''
     return abs(((xnp1 - xn) / xnp1))
 
 def compute_fx(x, a):
+     '''
+    input: integer base, integer exp
+    output: integer b
+
+    Computes f(x) = sin(x) - a
+    '''
     return math.sin(x) - a
 
 def compute_dfx(x):
+     '''
+    input: integer x, integer n, integer a, integer m
+    output: integer
+
+    Computes f'(x) = cos(x)
+    '''
     return math.cos(x)
 
 if __name__ == '__main__':
