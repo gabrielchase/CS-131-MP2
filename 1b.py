f014d59b96cc0fe4708b2a354b6bfd4e1b1ea962
gabrielchase <gchase.patron@gmail.com>
2019-05-03 17:55:34 +0800

    1b

---
+++ 1b.py
@@ -0,0 +1,24 @@
+import math
+
+def newton_raphson(a=0, x=.5, desired_percent_error=.01):
+    percent_error = 1
+    while (percent_error >= desired_percent_error):
+        xnp1 = x - (compute_fx(x, a) / compute_dfx(x))
+        percent_error = get_percent_error(xnp1, x)
+        x = xnp1
+        print('error%: ', percent_error)
+    return x
+
+def get_percent_error(xnp1, xn):
+    return abs(((xnp1 - xn) / xnp1))
+
+def compute_fx(x, a):
+    return math.sin(x) - a
+
+def compute_dfx(x):
+    return math.cos(x)
+
+if __name__ == '__main__':
+    a = float(input('A: '))
+    result = newton_raphson(a)
+    print('arcsin({}) = {}'.format(a, result))
